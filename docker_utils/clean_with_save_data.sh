d0d00be32131883cffa0d23d7bedc99c32778aba
Axewood <igor.zhugar@mail.ru>
2025-01-04 02:36:41 +0300

    update docker utils

--- docker_utils/clean_with_save_data.sh
+++ docker_utils/clean_with_save_data.sh
@@ -4,3 +4,4 @@ cd ..
 export $(grep -v '^#' .env | xargs)
 docker exec -t $(docker-compose ps -q db) pg_dump -U $DB_USER --data-only $DB_NAME > ./docker_utils/data_only_backup.sql
 docker-compose down --volumes --remove-orphans
+docker rmi axewood666/quiz_app:latest
